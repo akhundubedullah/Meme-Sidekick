fb01235a4510073fb25723e5eaff6d0beb91bdb4
akhundubedullah <akhundubedullah30@gmail.com>
2024-04-05 17:14:34 +0500

    Update smart_sidekick.py
    trying meme template integration with sentence

--- smart_sidekick.py
+++ smart_sidekick.py
@@ -1,1 +1,27 @@
 print("Hello, Meme Sidekick!")
+
+import random
+
+sentence = input("Write a funny sentence: ") 
+
+
+meme_templates = [
+    "Bad Luck Brian: [text]" ,
+    "Success Kid: [text] ",
+    "One Does Not Simply: [text]" 
+    # Hindi Templates
+    "Aeyin ye kiya howa? [text]", 
+    "Kiya mast joke mara re! [text]",
+    "Chup re halkat! [text]",
+    "Sahi pakde hain! [text]", 
+    # English Templates
+    "Nobody: [Silence] / [text]]", 
+    "[Common phrase] / Me, taking it literally: [text]" 
+    "When you finally [text]",
+    "Me as a programmer: [Expectation] / Me as a programmer: [Funny reality]"  
+] 
+
+
+chosen_template = random.choice(meme_templates)
+final_meme = chosen_template.replace("[text]", sentence)  # Simple insertion 
+print(final_meme)
