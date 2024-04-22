bc220bdad5bf2a12124746f06473ad98de28be52
akhundubedullah <akhundubedullah30@gmail.com>
2024-04-22 22:08:31 +0500

    Update smart_sidekick.py
    Gemnini Ai integration

--- smart_sidekick.py
+++ smart_sidekick.py
@@ -1,11 +1,15 @@
+import google.generativeai as genai
+import os 
 from textblob import TextBlob 
+import random
+
+
 
 
 print("Hello, Meme Sidekick!")
 
-import random
 
-sentence = input("Write a funny sentence: ") 
+meme_template = input("Wite a Meme ") # Get meme template here
 
 
 meme_templates = [
@@ -23,19 +27,19 @@ meme_templates = [
     "Me as a programmer: [Expectation] / Me as a programmer: [Funny reality]"  
 ] 
 
-chosen_template = random.choice(meme_templates)
-final_meme = chosen_template.replace("[text]", sentence)  # Simple insertion 
-print(final_meme)
+#chosen_template = random.choice(meme_templates)
+#final_meme = chosen_template.replace("[text]", sentence)  # Simple insertion 
+#print(final_meme)
 
 
 # Analysis
-num_words = len(sentence.split())  # Count the words
-has_exclamation = "!" in sentence   # Detect exclamation points
+num_words = len(meme_template.split())  # Count the words
+has_exclamation = "!" in meme_template   # Detect exclamation points
 print("Number of words:", num_words)
 print("Has exclamation:", has_exclamation)
 
 
-blob = TextBlob(sentence)
+blob = TextBlob(meme_template)
 sentiment = blob.sentiment.polarity  # Value between -1 (negative) and 1 (positive)
 
 if sentiment >= 0.5:
@@ -51,5 +55,11 @@ keywords = {
     "sarcasm": ["seriously", "obviously", "of course"],
 }
 for emotion, words in keywords.items():
-    if any(word in sentence.lower() for word in words):
-        print("Seems like the user is feeling", emotion)
+    if any(word in meme_template.lower() for word in words):
+     print("Seems like the user is feeling", emotion)
+
+genai.configure(api_key="AIzaSyBFthjm8lUCTy42FxhCEq3ytate0O7WTjU") 
+model = genai.GenerativeModel('gemini-pro')
+prompt = meme_template  # Use the meme template directly
+response = model.generate_content(prompt)
+print(response.text)  
